4dd1b857b2831fd0a311b46efd80adccd7920c8e
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-07-22 16:10:41 -0300

    feature: ProposalService class creation

---
+++ src/main/java/com/br/proposta_app/service/ProposalService.java
@@ -0,0 +1,11 @@
+package com.br.proposta_app.service;
+
+import com.br.proposta_app.dto.ProposalRequestDto;
+import com.br.proposta_app.dto.ProposalResponseDto;
+
+public class ProposalService {
+
+    public ProposalResponseDto createProposal(ProposalRequestDto requestDto) {
+        return null;
+    }
+}

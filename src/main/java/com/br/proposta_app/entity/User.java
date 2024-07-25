ca7418cdb769a93da31972f5beed2406916505c1
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-07-25 01:30:06 -0300

    refactor: imports and attributes refactor

--- src/main/java/com/br/proposta_app/entity/User.java
+++ src/main/java/com/br/proposta_app/entity/User.java
@@ -1,7 +1,16 @@
 package com.br.proposta_app.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
@@ -15,10 +24,10 @@ import java.util.UUID;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
-    private String lastname;
+    private String lastName;
     private String cpf;
     private String phone;
     private Double income;

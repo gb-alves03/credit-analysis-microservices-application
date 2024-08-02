f7723089e56b8fe6e43f9dff22c83513f14874d2
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-08-02 00:32:58 -0300

    feat: creation of the method to convert a entities list to Dtos list

---
+++ src/main/java/com/br/proposta_app/mapper/PropostaMapper.java
@@ -0,0 +1,38 @@
+package com.br.proposta_app.mapper;
+
+import com.br.proposta_app.dto.PropostaRequestDto;
+import com.br.proposta_app.dto.PropostaResponseDto;
+import com.br.proposta_app.entity.Proposta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface PropostaMapper {
+
+    PropostaMapper INSTANCE = Mappers.getMapper(PropostaMapper.class);
+
+    @Mapping(target = "usuario.nome", source = "nome")
+    @Mapping(target = "usuario.sobrenome", source = "sobrenome")
+    @Mapping(target = "usuario.cpf", source = "cpf")
+    @Mapping(target = "usuario.telefone", source = "telefone")
+    @Mapping(target = "usuario.renda", source = "renda")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "aprovado", ignore = true)
+    @Mapping(target = "integrada", ignore = true)
+    @Mapping(target = "observacao", ignore = true)
+    Proposta convertDtoToEntity(PropostaRequestDto requestDto);
+
+
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "sobrenome", source = "usuario.sobrenome")
+    @Mapping(target = "cpf", source = "usuario.cpf")
+    @Mapping(target = "telefone", source = "usuario.telefone")
+    @Mapping(target = "renda", source = "usuario.renda")
+    PropostaResponseDto convertEntityToDto(Proposta proposta);
+
+
+    List<PropostaResponseDto> convertListEntityToListDto(Iterable<Proposta> proposals);
+}

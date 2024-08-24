32d30df363b61156b8110bd5a774af470211a474
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-08-24 00:14:27 -0300

    feat: add the JsonBackReference annotation

--- src/main/java/com/br/proposta_app/entity/Usuario.java
+++ src/main/java/com/br/proposta_app/entity/Usuario.java
@@ -1,5 +1,6 @@
 package com.br.proposta_app.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,5 +32,6 @@ public class Usuario {
     private Double renda;
 
     @OneToOne(mappedBy = "usuario")
+    @JsonBackReference
     private Proposta proposta;
 }

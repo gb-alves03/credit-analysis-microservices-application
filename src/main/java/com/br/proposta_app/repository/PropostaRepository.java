4e3ff88824d902ca8b8b1c6ae73053df8c9c088f
gbalves03 <gabrielhenriquealves321@gmail.com>
2024-08-24 00:07:31 -0300

    feat: creation of the findAllByIntegradaIsFalse method

--- src/main/java/com/br/proposta_app/repository/PropostaRepository.java
+++ src/main/java/com/br/proposta_app/repository/PropostaRepository.java
@@ -4,8 +4,11 @@ import com.br.proposta_app.entity.Proposta;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PropostaRepository extends CrudRepository<Proposta, Long> {
 
+    List<Proposta> findAllByIntegradaIsFalse();
 
 }
